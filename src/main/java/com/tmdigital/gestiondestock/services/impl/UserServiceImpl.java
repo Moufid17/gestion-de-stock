d2feefc0e31947316891aafe053ae84267c80ffe
Moufid17 <moufid.mtr@gmail.com>
2024-12-30 23:38:34 +0100

    Service : User updated

--- src/main/java/com/tmdigital/gestiondestock/services/impl/UserServiceImpl.java
+++ src/main/java/com/tmdigital/gestiondestock/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tmdigital.gestiondestock.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -8,19 +9,23 @@ import org.springframework.stereotype.Service;
 import com.tmdigital.gestiondestock.dto.UserDto;
 import com.tmdigital.gestiondestock.exception.ErrorCodes;
 import com.tmdigital.gestiondestock.exception.InvalidEntityException;
+import com.tmdigital.gestiondestock.model.Company;
+import com.tmdigital.gestiondestock.repository.CompanyRepository;
 import com.tmdigital.gestiondestock.repository.UserRepository;
 import com.tmdigital.gestiondestock.services.UserService;
 import com.tmdigital.gestiondestock.validator.UserValidator;
 
-import io.micrometer.common.util.StringUtils;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, CompanyRepository companyRepository) {
         this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Override
@@ -29,6 +34,11 @@ public class UserServiceImpl implements UserService {
         
         if (!errors.isEmpty()) {
             throw new InvalidEntityException("L'utilisateur n'est pas valide", ErrorCodes.USER_NOT_VALID, errors);
+        }
+
+        Optional<Company> company = companyRepository.findById(dto.getCompany().getId());
+        if (!company.isPresent()) {
+            throw new InvalidEntityException("Aucune société a'existe pas.", ErrorCodes.COMPANY_NOT_FOUND);
         }
 
         return UserDto.fromEntity(userRepository.save(UserDto.toEntity(dto)));
@@ -47,7 +57,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto findByEmail(String email) {
-        if (email == null || StringUtils.isEmpty(email)) {
+        if (email == null || !StringUtils.hasLength(email)) {
             throw new InvalidEntityException("Aucun email n'a été fourni");
         }
 
