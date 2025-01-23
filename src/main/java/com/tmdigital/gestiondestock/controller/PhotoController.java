8b9f1f95fb975af65afccb443591ab0dfca9a5d7
Moufid17 <moufid.mtr@gmail.com>
2025-01-23 11:38:29 +0100

    init swagger (Hidden photo endpoint)

--- src/main/java/com/tmdigital/gestiondestock/controller/PhotoController.java
+++ src/main/java/com/tmdigital/gestiondestock/controller/PhotoController.java
@@ -21,7 +21,7 @@ public class PhotoController implements PhotoApi {
     }
 
     @Override
-    public Object savePhoto(String context, Integer id, MultipartFile photo, String title) throws IOException, FlickrException {
+    public Object savePhoto(MultipartFile photo, Integer id, String context,  String title) throws IOException, FlickrException {
         return strategyPhotoContext.savePhoto(context, id, photo.getInputStream(), title);
     }
 
--- src/main/java/com/tmdigital/gestiondestock/controller/api/ArticleApi.java
+++ src/main/java/com/tmdigital/gestiondestock/controller/api/ArticleApi.java
@@ -10,11 +10,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tmdigital.gestiondestock.dto.ArticleDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
+@Tag(name = "Article", description = "Article API")
 public interface ArticleApi {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create an article", description = "Allow to create an new article", 
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Article created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Article already exists", content = @Content)
+        }
+    )
     ArticleDto save(@RequestBody ArticleDto dto);
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
