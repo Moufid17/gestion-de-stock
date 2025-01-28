ae1a0d78116b76de448d590fce4cdbcdee71bbd5
Moufid17 <moufid.mtr@gmail.com>
2025-01-28 03:54:07 +0100

    update api docs (photo endpoint can be trigger)

--- src/main/java/com/tmdigital/gestiondestock/controller/api/PhotoApi.java
+++ src/main/java/com/tmdigital/gestiondestock/controller/api/PhotoApi.java
@@ -3,8 +3,12 @@ package com.tmdigital.gestiondestock.controller.api;
 import com.flickr4java.flickr.FlickrException;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.IOException;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -12,9 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface PhotoApi {
 
-  @PostMapping("/{id}/{title}/{context}")
+  @PostMapping(value="/{id}/{title}/{context}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   Object savePhoto(
-    @Parameter(description = "file") @RequestPart("file") MultipartFile photo, 
+    @Parameter(description = "file", 
+      content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", format = "binary"))) 
+      @RequestPart("file") MultipartFile photo, 
     @Parameter(description = "Object identifier") @PathVariable("id") Integer id,
     @Parameter(description = "File name") @PathVariable("title") String title,
     @Parameter(description = "File context") @PathVariable("context") String context
