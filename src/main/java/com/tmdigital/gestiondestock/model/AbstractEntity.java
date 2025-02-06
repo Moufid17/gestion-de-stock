2337d957358ce6f384968c845faff5cf84c2af68
Moufid17 <moufid.mtr@gmail.com>
2025-02-06 01:34:44 +0100

    fix date event listener error

--- src/main/java/com/tmdigital/gestiondestock/model/AbstractEntity.java
+++ src/main/java/com/tmdigital/gestiondestock/model/AbstractEntity.java
@@ -3,8 +3,8 @@ package com.tmdigital.gestiondestock.model;
 import java.io.Serializable;
 import java.time.Instant;
 
-// import org.springframework.data.annotation.CreatedDate;
-// import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -14,8 +14,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Data;
 
 @Data
@@ -29,23 +27,13 @@ public abstract class AbstractEntity implements Serializable {
     @GeneratedValue
     private Integer id;
 
-    // @CreatedDate
+    @CreatedDate
     @Column(name = "createAt", nullable = false, updatable = false)
     @JsonIgnore
     private Instant createAt;
 
-    // @LastModifiedDate
+    @LastModifiedDate
     @Column(name = "updateAt")
     @JsonIgnore
     private Instant updateAt;
-
-    @PrePersist
-    void beforeCreate() {
-        this.createAt = Instant.now();
-    }
-
-    @PreUpdate
-    void beforeUpdate() {
-        this.updateAt = Instant.now();
-    }
 }
