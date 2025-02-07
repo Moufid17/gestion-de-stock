dd1af04659237126be3703ad4e2ac21f85721fbe
Moufid17 <moufid.mtr@gmail.com>
2025-02-07 01:58:34 +0100

    Model, Service and Controller (User) : update mapping with company

--- src/main/java/com/tmdigital/gestiondestock/controller/api/UserApi.java
+++ src/main/java/com/tmdigital/gestiondestock/controller/api/UserApi.java
@@ -11,19 +11,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tmdigital.gestiondestock.dto.UserDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+@Tag(name = "User", description = "The user API")
 public interface UserApi {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     UserDto save(@RequestBody UserDto dto);
 
+    @GetMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<UserDto> findAll();
+
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retreive an user", description = "Allow to retreive an user by his id", 
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Retreive an user with success"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Client not found"),
+        }
+    )
     UserDto findById(@PathVariable Integer id);
 
-    @GetMapping(value = "/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserDto findByEmail(@PathVariable String email);
-
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    List<UserDto> findAll();
+    @GetMapping(value="/email/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+    UserDto findByEmail(@PathVariable("email") String email);
 
     @GetMapping(value = "/company/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     List<UserDto> findAllByCompany(@PathVariable("id") Integer idCompany);
