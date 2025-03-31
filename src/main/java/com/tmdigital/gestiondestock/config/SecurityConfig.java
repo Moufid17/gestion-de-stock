e5a82a729efada70f39e15301b0fdfc660aca58f
Moufid17 <moufid.mtr@gmail.com>
2025-03-31 19:09:44 +0200

    Controller : Make company creation public

--- src/main/java/com/tmdigital/gestiondestock/config/SecurityConfig.java
+++ src/main/java/com/tmdigital/gestiondestock/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         final String[] routesAllowed = {
             APP_ROOT+ "/register",
             APP_ROOT+ "/authenticate",
-            APP_ROOT+ "/companies",
+            APP_ROOT+ "/public/**",
             // // SWAGGER
             "/v3/api-docs/**",
             "/swagger-ui/**",
