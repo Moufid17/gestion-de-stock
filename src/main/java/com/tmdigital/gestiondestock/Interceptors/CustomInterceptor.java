74be52b63a89e6d288f674e5307f781f369aca1a
Moufid17 <moufid.mtr@gmail.com>
2025-04-01 21:04:06 +0200

    Migration from mysql to postgres : postgres db often execute sequential sql request"select nextval(...)". so have to handle this case here. Change "idCompany" to "companyId).

--- src/main/java/com/tmdigital/gestiondestock/Interceptors/CustomInterceptor.java
+++ src/main/java/com/tmdigital/gestiondestock/Interceptors/CustomInterceptor.java
@@ -7,21 +7,29 @@ import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.slf4j.MDC;
 import org.springframework.util.StringUtils;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 public class CustomInterceptor implements StatementInspector {
 
     private static final long serialVersionUID = 1L;
 
     @Override
     public String inspect(String sql) {
-        final ArrayList<String> excludedEntities = new ArrayList<>(Arrays.asList("company", "roles", "roles_appusers", "appusers_roles"));
-        
+        final ArrayList<String> excludedEntities = new ArrayList<>(Arrays.asList("company", "roles", "roles_users", "users_roles"));
+            
+
         if (sql.toLowerCase().startsWith("select")) {
-            String idCompany = MDC.get("idCompany");
-            if (idCompany == null) return sql;         
-            // Handle the case when the idCompany is a master company. Try to find a better way to handle this case.       
-            if ("200".equals(idCompany)) return sql;               
+
+            if (sql.contains("nextval") || sql.contains("currval") || sql.contains("setval")) {
+                return sql;
+            }
+
+            String companyId = MDC.get("companyId");
+            if (companyId == null) return sql;         
+            // Handle the case when the company id is a master company. Try to find a better way to handle this case.       
+            if ("200".equals(companyId)) return sql;               
             
             Integer indexOfFromInSql = sql.indexOf("from") + "from".length();
             String entityName = sql.substring(indexOfFromInSql+1).toLowerCase().split(" ")[0];
@@ -29,16 +37,16 @@ public class CustomInterceptor implements StatementInspector {
 
             if (StringUtils.hasLength(entityName)
                 && !excludedEntities.contains(entityName)
-                && StringUtils.hasLength(idCompany)) 
+                && StringUtils.hasLength(companyId)) 
             {
                 if (sql.contains("where")) {
-                    sql = sql + " and " + sqlEntityName + ".idcompany=" + idCompany;
+                    sql = sql + " and " + sqlEntityName + ".company_id=" + companyId;
                 } else {
-                    sql = sql + " where " + sqlEntityName +".idcompany=" + idCompany; 
+                    sql = sql + " where " + sqlEntityName +".company_id=" + companyId; 
                 }
             }
         }
-            
+
         return sql;
     }  
 
